d4bd844d34d19c99ff46b4e837ce6999dcca781d
CasterWx <antzuhl1998@gmail.com>
2019-12-26 18:02:01 +0800

    rpc

--- zeus-rpc/src/main/java/com/antzuhl/zeus/registry/ServiceRegistry.java
+++ zeus-rpc/src/main/java/com/antzuhl/zeus/registry/ServiceRegistry.java
@@ -1,23 +1,19 @@
 package com.antzuhl.zeus.registry;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+
+import com.antzuhl.zeus.cache.ServerCache;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * 服务注册
- *
- * @author huangyong
- * @author luxiaoxun
- */
 public class ServiceRegistry {
 
     private static final Logger logger = LoggerFactory.getLogger(ServiceRegistry.class);
@@ -34,7 +30,6 @@ public class ServiceRegistry {
         if (data != null) {
             ZooKeeper zk = connectServer();
             if (zk != null) {
-                AddRootNode(zk); // Add root node if not exist
                 createNode(zk, data);
             }
         }
@@ -54,36 +49,47 @@ public class ServiceRegistry {
             latch.await();
         } catch (IOException e) {
             logger.error("", e);
-        }
-        catch (InterruptedException ex){
+        } catch (InterruptedException ex){
             logger.error("", ex);
         }
         return zk;
-    }
-
-    private void AddRootNode(ZooKeeper zk){
-        try {
-            Stat s = zk.exists(Constant.ZK_REGISTRY_PATH, false);
-            if (s == null) {
-                zk.create(Constant.ZK_REGISTRY_PATH, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
-            }
-        } catch (KeeperException e) {
-            logger.error(e.toString());
-        } catch (InterruptedException e) {
-            logger.error(e.toString());
-        }
     }
 
     private void createNode(ZooKeeper zk, String data) {
         try {
             byte[] bytes = data.getBytes();
             String path = zk.create(Constant.ZK_DATA_PATH, bytes, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            logger.debug("create zookeeper node ({} => {})", path, data);
+            logger.info("create zookeeper node ({} => {})", path, data);
         } catch (KeeperException e) {
             logger.error("", e);
         }
         catch (InterruptedException ex){
             logger.error("", ex);
+        }
+    }
+
+    public List<String> getAllNode(){
+        ZooKeeper zk = connectServer();
+        if (zk == null){
+            return null;
+        }
+        List<String> nodes = null;
+        try {
+            nodes = zk.getChildren(Constant.ZK_DATA_PATH, false);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return nodes;
+    }
+
+    public static void main(String[] args) {
+        ServiceRegistry serviceRegistry = new ServiceRegistry("127.0.0.1:2181");
+        serviceRegistry.register("coa");
+        serviceRegistry.register("sos");
+        while (true) {
+
         }
     }
 }
