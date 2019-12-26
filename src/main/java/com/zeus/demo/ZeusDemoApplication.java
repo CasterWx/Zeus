ee8a8eb09b853e8e9843444c8efff9f8c813ed57
CasterWx <antzuhl1998@gmail.com>
2019-12-26 15:27:20 +0800

    init project

---
+++ src/main/java/com/zeus/demo/ZeusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zeus.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeus/demo/ZeusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
